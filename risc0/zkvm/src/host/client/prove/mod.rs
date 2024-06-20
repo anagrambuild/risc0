05d44dc0f9ed900dabdc5c2990126a4d4d00f9d8
austbot <me@austbot.com>
2024-06-20 12:17:00 -0500

    put bonsai under a feature flag

--- risc0/zkvm/src/host/client/prove/mod.rs
+++ risc0/zkvm/src/host/client/prove/mod.rs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+#[cfg(feature = "bonsai")]
 pub(crate) mod bonsai;
 pub(crate) mod external;
 #[cfg(feature = "prove")]
